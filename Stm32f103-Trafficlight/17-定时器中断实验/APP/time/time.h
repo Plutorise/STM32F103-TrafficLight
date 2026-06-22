77f05fdb954cc0c2d7d4c72b2571a6d325adf8dd
Plutorise <141013278+Plutorise@users.noreply.github.com>
2026-06-22 09:52:59 +0800

    Add state definitions for traffic light control

--- Stm32f103-Trafficlight/17-定时器中断实验/APP/time/time.h
+++ Stm32f103-Trafficlight/17-定时器中断实验/APP/time/time.h
@@ -3,6 +3,21 @@
 
 #include "system.h"
 
+/* 工作状态定义 */
+typedef enum {
+	NORMAL_MODE,      /* 正常模式 */
+	EMERGENCY_MODE    /* 紧急模式 */
+} WorkMode_TypeDef;
+
+/* 交通灯状态定义 */
+typedef enum {
+	GREEN_ON,         /* 绿灯亮 */
+	YELLOW_FLASH,     /* 黄灯闪烁 */
+	RED_ON            /* 红灯亮 */
+} TrafficState_TypeDef;
+
 void TIM4_Init(u16 per,u16 psc);
+void EXTI_PA0_Init(void);
+void Traffic_Light_Manager(void);
 
 #endif
